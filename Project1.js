24930483cd107d32c771c589abf681df77942876
1511ish <117707870+1511ish@users.noreply.github.com>
2023-11-20 22:51:07 +0530

    forth commit
    It is very small project where if i type something in the input field and click the submit button than  the input will show at the top.
    Here we using Buffer class.

---
+++ Project1.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+
+const Server = http.createServer();
+
+Server.on('request', (req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="variable"></input><button type="submit">submit</button></form></body>')
+        res.write('</html>')
+        res.end();
+        return;
+    }
+
+    else if (url === '/message' && req.method === 'POST') {
+        console.log("message Page");
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            // fs.writeFileSync('message.txt', message);
+            // console.log(message);
+            // res.statusCode = 302;
+            res.write('<html>')
+            res.write('<head><title>Enter Message</title></head>')
+            res.write('<body>')
+            res.write(`<h3>${message}</h3>`);
+            res.write('<form action="/message" method="POST"><input type="text" name="variable"></input><button type="submit">submit</button></form>')
+            res.write('</body>')
+            res.write('</html>')
+            res.end();
+        })
+
+    }
+})
+
+Server.listen(4000);
